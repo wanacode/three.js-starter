20ca89fc34ce949af6bf10aa3793210a3c026650
Fraser <frasernz@gmail.com>
2021-04-08 08:53:29 -0500

    Fix shader text

--- src/shader/fragment.glsl
+++ src/shader/fragment.glsl
@@ -1,1 +1,16 @@
-†Ûi³ÿî¦ZvÇ(uëÖèÆ*?º™hiÛ?ºÇ«ı×ywİy¯}ÛµïŞ·ï¦¼Ñ¾;éŞôãòpWëj	Ø%²
+uniform float time;
+uniform float progress;
+uniform sampler2D t;
+uniform sampler2D displace;
+uniform vec4 resolution;
+varying vec2 vUv;
+
+void main()	{
+
+  vec2 displacedUV = vec2(vUv.x + 0.1 * sin(vUv.y * 19.), vUv.y);
+  vec4 color = texture2D(t, displacedUV);
+
+  // vec4 color = texture2D(t, vUv);
+
+  gl_FragColor = color;
+}
--- src/shader/vertex.glsl
+++ src/shader/vertex.glsl
@@ -1,1 +1,10 @@
-†Ûi³ÿî¦ZvÇ(uëÖèÆ*?º™hiÛ?ºÇ«ı×ywİy¯}ÛµïŞ·ï¦¼Ñ¾;éŞôãõâ©{Ş®×±‚[%
+uniform float time;
+varying vec2 vUv;
+varying vec3 vPosition;
+uniform vec2 pixels;
+float PI = 3.141592653589793238;
+
+void main() {
+  vUv = uv;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
